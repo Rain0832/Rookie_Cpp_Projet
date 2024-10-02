f1bbc82f52538f4a887b33e5941edbb90d961594
Rain Sugant <2710007824@qq.com>
2024-10-02 13:29:47 +0800

    upload file

---
+++ Project1_EMS/boss.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "worker.h"
+
+using namespace std;
+
+//总裁
+class Boss : public Worker
+{
+public:
+
+    Boss(int id, string name, int dId);
+
+    //显示个人信息
+    virtual void showInfo();
+
+    //获取岗位名称
+    virtual string getDeptName();
+
+};
